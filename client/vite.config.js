e8ceb642cd8c5f8d053ab78ff006d081b5176163
Hussnain Butt <121555081+Hussnain-Butt@users.noreply.github.com>
2025-02-13 21:04:47 +0500

    some vite settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    port: 8080
+  },
+  build: {
+    outDir: 'dist'
   },
   resolve: {
     alias: {

052822bc423ae0c83026ff9632f642801674b674
Hussnain Butt <121555081+Hussnain-Butt@users.noreply.github.com>
2025-02-16 11:34:49 +0500

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const { signup, login,forgotPassword,resetPassword} = require('../controller/authController');
+const { login,forgotPassword,resetPassword} = require('../controller/authController');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
-router.post('/signup', signup);
 router.post('/login', login);
 router.post('/forgot-password', forgotPassword);
 router.put('/reset-password/:token', resetPassword);
